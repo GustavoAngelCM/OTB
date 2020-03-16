9439a22a70f0f913cf7bc20141cd880e4b397fee
NIRBIT\crgt7 <crgt7_8@hotmail.com>
2020-03-16 15:20:25 -0400

    cors

--- config/database.php
+++ config/database.php
@@ -55,11 +55,11 @@ return [
             'password' => $DATABASE_URL['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_general_ci',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'MyISAM',
+            'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
