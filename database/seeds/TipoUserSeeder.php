be0be1ddedaae791733b60f6581d67a00e861186
NIRBIT\crgt7 <crgt7_8@hotmail.com>
2020-03-16 20:01:56 -0400

    db seeder

---
+++ database/seeds/TipoUserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TipoUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('tipo_usuarios')->insert([
+            [
+                'idTipoUsuario' => 1,
+                'nombreTipoUsuario' => 'ADMINISTRADOR',
+            ],
+            [
+                'idTipoUsuario' => 2,
+                'nombreTipoUsuario' => 'SOCIO',
+            ],
+            [
+                'idTipoUsuario' => 3,
+                'nombreTipoUsuario' => 'DIRECTIVO',
+            ],
+        ]);
+    }
+}
